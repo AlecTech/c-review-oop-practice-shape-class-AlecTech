cdaacc0f266ed78e825da345ec8347bebd2b6c2c
AlecTech <eremeevoleg@gmail.com>
2020-10-23 16:08:04 -0600

    Added empty abstract Shape class with empty properties;

--- oct23ShapesClassesPracticeCS/Shapes/Shape.cs
+++ oct23ShapesClassesPracticeCS/Shapes/Shape.cs
@@ -4,7 +4,45 @@ using System.Text;
 
 namespace oct23ShapesClassesPracticeCS.Shapes
 {
-    class Shape
+    public abstract class Shape
     {
+        private float _area;
+        private string _color;
+        private float _perimeter;
+        public float Area
+        {
+            get
+            {
+                return _area;
+            }
+           // set
+          //  {
+          //      _area = value;
+           // }
+        }
+        public float Perimeter
+        {
+            get
+            {
+                return _perimeter;
+            }
+            //set
+           // {
+           //     _perimeter = value;
+           // }
+        }
+        public string Color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                _color = value;
+            }
+        }
+        public abstract void CalculateArea();
+        public abstract void CalculatePerimeter();
     }
 }
