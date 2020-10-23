a139675f27c7491f74af99cdea525fd82636f59e
AlecTech <eremeevoleg@gmail.com>
2020-10-23 14:23:30 -0600

    initial commit;

---
+++ oct23ShapesClassesPracticeCS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oct23ShapesClassesPracticeCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
