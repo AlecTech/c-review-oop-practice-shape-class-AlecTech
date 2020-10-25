117495ab6228b4738a07794522a596c01c9b8931
AlecTech <eremeevoleg@gmail.com>
2020-10-25 16:44:30 -0600

    Re-format triangle class to return area and perimeter formulas from the overridden properies inside get;

--- oct23ShapesClassesPracticeCS/Shapes/Triangle.cs
+++ oct23ShapesClassesPracticeCS/Shapes/Triangle.cs
@@ -4,28 +4,46 @@ using System.Text;
 
 namespace oct23ShapesClassesPracticeCS.Shapes
 {
-    /*
     public class Triangle : Shape
     {
-        
         public double Base { get; set; }
         public double Height { get; set; }
-
-        public Triangle(float area, float perimeter, string color)
-       : base(area, perimeter, color)
-        {
-
-            Color = color;
-        }
-        public override void CalculateArea()
-        {
-            Area = (Base * Height)/2;
-        }
-        public override void CalculatePerimeter()
-        {
-            Perimeter = Base+Math.Sqrt((Base*Base) + 4*(Height*Height));
-        }
         
+        //inheritance
+        public Triangle(string color)
+            : base(color)
+        {
+
+            //overridden constructor
+        }
+        //pipeline data to base class
+        public Triangle(double abase, double height, string color)
+           : base(color)
+        {
+            Base = abase;
+            Height = height;
+        }
+        public Triangle() : base()
+        {
+            //default constructor
+        }
+        public override double Area
+        {
+            get
+            {
+                return (Base * Height) / 2;
+            }
+        }
+
+        public override double Perimeter
+        {
+            get
+            {
+                return Base + Math.Sqrt((Base * Base) + 4 * (Height * Height));
+            }
+        }
+
+     
     }
-        */
+
 }
